5a84a55ddaf04133804656d5bd4c334c743dd301
monkeyWangs <2424880409@qq.com>
2018-01-03 15:49:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="layout">
+    <% _.forEach(data.components, function(component){ %>
+      <<%- component.name %> :data="component.data"></<%- component.name %>>
+    <% }) %>
+  </div>
+</template>
+
+<script>
+  <% _.forEach(data.components, function(component){ %>
+    import t{{component.name}} from 'components/{{component.source}}.vue'
+  <% }) %>
+export default {
+  name: 'app',
+  components: {
+  <% _.forEach(data.components, function(component){ %>
+    {{component.name}},
+  <% }) %>
+  }
+}
+</script>
+
+<style lang="scss">
+  .header {
+    width: 100%;
+    height: 50px;
+    border: 1px solid blue;
+  }
+  .footer {
+    width: 100%;
+    height: 50px;
+    border: 1px solid red;
+  }
+  .container {
+    width: 100%;
+    height: 500px;
+    border: 1px solid black;
+  }
+</style>
---
+++ src/components/tmp2/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+    {{data.msg}}
+  </div>
+</template>
+<script>
+  export default{
+    props: {
+      data: {
+        type: Object,
+        default: {}
+      }
+    }
+  }
+</script>
